49f649eac28580bd936399bed8593fc9ab36ff27
sergey.suslov <sergey.suslov.nsk@gmail.com>
2020-01-12 14:18:12 +0700

    chore: pino logger

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express'
 import config from 'config'
 import { config as dotconfig } from 'dotenv'
-
 dotconfig({ path: '.env' })
+import expressPinoLogger from 'express-pino-logger'
+import { loggerExpress, injectLogger } from './utils/logger'
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Ping'))
+app.use(expressPinoLogger({ logger: loggerExpress }))
+app.use(injectLogger)
+
+app.get('/', (req, res) => {
+  res.send('Ping')
+})
 
 app.listen(config.get('port') || 3000, () => console.log('Running on port', config.get('port') || 3000))
